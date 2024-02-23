f3bec707d03cb23377668f9a7cad1e9f5c5b98fb
Caris <cwill1013@gmail.com>
2024-02-23 00:01:40 -0500

    Initial Commit

--- app.js
+++ app.js
@@ -3,8 +3,41 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+// Exercise 1 & 2
+const firstName = "Caris";
 
-// Exercise 2
+let lastName = "Williams";
+
+var age = 42;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995
+let iCaseSensitive = "true";
+console.log(`Hey ${firstName} ${lastName} what made you want to learn ${language} at the age of ${age}? ${language} was created in ${createdYear}.`);
+
+// Exercise 4
+let price = 19.99;
+
+let isOnSale = "false";
+
+let salePercentage = 15;
+
+let stock = 0 
+
+let inStock = "false"
+
+let selectedSize = `M`
+console.log(`How much are these? Sales Associte states that they are ${price}. Customer says I heared they weree on sale. Sale Associte that is ${isOnSale}. Well can you give me like ${salePercentage} dollars off. Sales Associte that will be ${inStock}`)
+// Exercise 5
+
+let title = `Name of the Wind`;
+
+let author = `Patrick Rothfuss`;
+
+let pageCount = 722;
+
+let bookmark = 456;
+
+let hasRead = `true`
+console.log(`I'm reading ${title} by ${author}. I am on page ${bookmark} there's a total of ${pageCount} pages.`)
